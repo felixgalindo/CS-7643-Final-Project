d95310753f57cd952807cb552087973530898aad
Felix <felixgalindo91@gmail.com>
2024-11-27 18:05:12 -0600

    Reset

---
+++ imgFeatureExtractor.py
@@ -0,0 +1,76 @@
+import os
+import io
+import torch
+from tqdm import tqdm
+from PIL import Image
+from torchvision import models, transforms
+from waymo_open_dataset import dataset_pb2 as open_dataset
+import tensorflow as tf
+
+
+class ImgFeatureExtractor:
+    def __init__(self, inDir, outDir, model):
+        self.inDir = inDir
+        self.outDir = outDir
+
+        self.imgTransformer = transforms.Compose([
+            transforms.Resize((224, 224)),  # Resize image 224x224
+            transforms.ToTensor(),  # Converts PIL image into PyTorch tensor
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize image
+        ])
+
+        # Load the pre-trained model and remove the last two layers (AvgPool and Fully Connected)
+        self.model = model
+        self.model = torch.nn.Sequential(*list(model.children())[:-2])
+        self.model.eval()
+
+    def extractFeatures(self, imagePath):
+        """
+        Extracts features from an image file.
+
+        :param imagePath: Path to the image file.
+        :return: Numpy array containing features.
+        """
+        image = Image.open(imagePath).convert("RGB")  # Open image from file path
+        imageTensor = self.imgTransformer(image).unsqueeze(0)  # Transform and add batch dimension
+
+        with torch.no_grad():
+            features = self.model(imageTensor)
+            features = features.squeeze().numpy()  # Remove batch dimension
+        print("Features extracted!")
+        return features
+
+
+    def parseJPGImages(self):
+        """
+        Extract features from all `.jpg` images in the input directory and subdirectories
+        and save them as PyTorch Tensors
+        """
+        for root, dirs, files in os.walk(self.inDir):
+            image_files = [f for f in files if f.endswith('.jpg')]
+
+            for imageFile in tqdm(image_files, desc=f"Processing Images in {root}"):
+                imagePath = os.path.join(root, imageFile)
+                features = self.extractFeatures(imagePath)
+
+                relativePath = os.path.relpath(root, self.inDir)
+                outputDir = os.path.join(self.outDir, relativePath)
+                os.makedirs(outputDir, exist_ok=True)
+
+                outputFile = os.path.join(outputDir, imageFile.replace('.jpg', '.pt'))
+                torch.save(features, outputFile)
+
+                print(f"Features saved to {outputFile}")
+
+
+if __name__ == "__main__":
+    inDir = os.path.expanduser(os.path.dirname(os.path.abspath(__file__)) +"/dataset/compressed_camera_images")
+    outDir = os.path.expanduser(os.path.dirname(os.path.abspath(__file__)) + "/data/compressed_camera_images")
+
+    os.makedirs(outDir, exist_ok=True)
+
+    # Initialize the feature extractor
+    featureExtractor = WaymoImgFeatureExtractor(inDir, outDir, models.resnet50(pretrained=True))
+
+    # Process all images 
+    featureExtractor.parseJPGImages()
---
+++ pedestrialDetector.py
@@ -0,0 +1,62 @@
+import os
+import io
+import torch
+import torch.nn as nn
+import pickle
+
+
+class PedestrianDetector(nn.Module):
+    def __init__(self, modelDim=256, nClasses=2, nHeads=8,nLayers=6 ):
+        super(PedestrianDetector, self).__init__()
+
+        #Create embedding projectors
+        self.cnnProjector = nn.Linear(2048, modelDim)
+        #self.lidarProjector = nn.Linear(2048, modelDim)
+
+        #Transformer
+        self.transformer = nn.TransformerEncoder(nn.TransformerEncoderLayer(modelDim,nHeads),nLayers)
+
+        #Create predictors
+        self.boxPredictor = nn.Linear(modelDim,6) # x,y,x,len,width,height
+        self.classPredictor = nn.Linear(modelDim,2) # Pedistrian, non-pedstrian
+
+        #Positional Encoder
+        self.positionalEncoder = nn.Parameter(torch.zeros(500), modelDim)
+
+    def forward(self, cameraFeatures):#, lidarFeatures):
+
+        #Create embeddings
+        cameraEmbeddings = self.cnnProjector(cameraFeatures)
+        #lidarEmbeddings = self.cnnProjector(lidarFeatures)
+        fusedEmbeddings = cameraEmbeddings
+        #fusedEmbeddings = torch.cat([cameraEmbeddings,lidarEmbeddings], dim=1)
+
+        #Add the positional encodings
+        fusedEmbeddings = self.positionalEncoder[:fusedEmbeddings.size(1)]
+
+        #Get transformer output and make predictions
+        transformerOut = self.transformer(fusedEmbeddings)
+        classes = self.classPredictor(transformerOut)
+        boxes = self.boxPredictor(transformerOut)
+
+        return classes,boxes
+
+if __name__ == "__main__":
+    #inDir = os.path.expanduser(os.path.dirname(os.path.abspath(__file__)) +"/dataset/compressed_camera_images")
+    #outDir = os.path.expanduser(os.path.dirname(os.path.abspath(__file__)) + "/data/compressed_camera_images")
+    pickleFile = os.path.expanduser(os.path.dirname(os.path.abspath(__file__)) +"/dataset/camera_box_list_20241127.pkl")
+
+    #os.makedirs(outDir, exist_ok=True)
+    # Open the .pkl file in read-binary mode
+    print(pickleFile)
+    with open(pickleFile, 'rb') as file:
+        data = pickle.load(file)
+
+    for d in data:
+        print(d)
+
+    # groundTruth = []
+    # # Initialize the Pedestrian Detector
+    # featureExtractor = PedestrianDetector(groundTruth)
+
+
