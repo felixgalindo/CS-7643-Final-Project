490dc163ed75722e1328092537fcb184b1b68fa5
YWang <ywang@gatech.edu>
2024-12-08 11:03:21 -0800

    Included the change.

--- baseline_model_maskRCNN.py
+++ baseline_model_maskRCNN.py
@@ -4,11 +4,14 @@ from torchvision.models.detection import maskrcnn_resnet50_fpn
 from torchvision.transforms import functional as F
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
 import os
+import glob
+import pickle
+from tqdm import tqdm
+
 # Load Pretrained Mask R-CNN model
 model = maskrcnn_resnet50_fpn(pretrained=True)
-model.eval()  
+model.eval()
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 model.to(device)
@@ -19,7 +22,7 @@ def load_image(image_path):
     # Convert to tensor and normalize
     img_tensor = F.to_tensor(img).unsqueeze(0).to(device)  # Add batch dimension
     return img, img_tensor
-def get_predictions(img_tensor, threshold=0.7):
+def get_predictions(img_tensor):
     # Get model predictions
     outputs = model(img_tensor)[0]
 
@@ -27,14 +30,13 @@ def get_predictions(img_tensor, threshold=0.7):
     pred_boxes = outputs['boxes'].detach().cpu().numpy()
     pred_scores = outputs['scores'].detach().cpu().numpy()
     pred_classes = outputs['labels'].detach().cpu().numpy()
-    pred_masks = outputs['masks'].detach().cpu().numpy()
 
     # Keep only predictions above the threshold and 3 as cars
-    high_conf_indices = (pred_scores > threshold) & (pred_classes == 3)
+    high_conf_indices = pred_classes == 3
     pred_boxes = pred_boxes[high_conf_indices]
     pred_classes = pred_classes[high_conf_indices]
-    pred_masks = pred_masks[high_conf_indices]
-    return pred_boxes, pred_classes, pred_masks
+
+    return pred_boxes, pred_classes, pred_scores
 def visualize_predictions(img, boxes, masks, labels, class_names):
     # Apply masks and draw bounding boxes on the image
     for i, box in enumerate(boxes):
@@ -56,7 +58,7 @@ def visualize_predictions(img, boxes, masks, labels, class_names):
     plt.show()
 # Define class names for COCO dataset
 COCO_CLASSES = [
-    "__background__", "person", "bicycle", "car", "motorcycle", "airplane", 
+    "__background__", "person", "bicycle", "car", "motorcycle", "airplane",
     "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign",
     "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
     "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag",
@@ -71,28 +73,36 @@ COCO_CLASSES = [
 ]
 
 # Function to process a folder of images and return bounding boxes
-def process_image_folder(folder_path):
-    all_boxes = {}  # Dictionary to store bounding boxes for each image
-    
-    for filename in os.listdir(folder_path):
-        image_path = os.path.join(folder_path, filename)
-        
-        if image_path.endswith(".jpg") or image_path.endswith(".png"):
-            print(f"Processing {filename}...")
-            
-            original_img, img_tensor = load_image(image_path)
-            # Get bounding boxes for the current image
-            boxes, labels, masks = get_predictions(img_tensor, threshold=0.7)
-            all_boxes[filename] = boxes
-            
-            # Optionally visualize the bounding boxes
-            # visualize_predictions(original_img, boxes, masks, labels, COCO_CLASSES)
-    
-    return all_boxes
+def process_image_folder(image_path):
+
+    original_img, img_tensor = load_image(image_path)
+    # Get bounding boxes for the current image
+    boxes, labels, scores = get_predictions(img_tensor)
+
+    output = {
+        "boxes": boxes,
+        "scores": scores
+    }
+    # Optionally visualize the bounding boxes
+    # visualize_predictions(original_img, boxes, masks, labels, COCO_CLASSES)
+    return output
+
 
 # path to your folder
-folder_path = r"C:\Users\pwang\OneDrive\Documents\GitHub\CS-7643-Final-Project\sample_images"
-process_image_folder(folder_path)
-# # Get predictions
-# boxes, labels, masks = get_predictions(img_tensor, threshold=0.7)
-# print(boxes)
+folder_path = "/home/meowater/Documents/ssd_drive/compressed_camera_images2/"
+box_path = '/home/meowater/Documents/ssd_drive/maskRCNN_boxes/'
+
+img_list = glob.glob(os.path.join(folder_path, '*/*.jpg'), recursive=True)
+
+for fn in tqdm(img_list):
+    file_path, base_name = os.path.split(fn)
+    context_name = file_path.split('/')[-1]
+
+    name_prefix = context_name + '_' + base_name.split('.')[0]
+
+    sub_save_path = os.path.join(box_path, context_name)
+    os.makedirs(sub_save_path, exist_ok=True)
+    new_fn = os.path.join(sub_save_path, name_prefix + '_maskCNN.pkl')
+    box_output = process_image_folder(fn)
+    with open(new_fn, 'wb') as handle:
+        pickle.dump(box_output, handle, protocol=pickle.HIGHEST_PROTOCOL)
